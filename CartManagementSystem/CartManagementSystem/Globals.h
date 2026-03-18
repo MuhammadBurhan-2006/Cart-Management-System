dc3c1a2a45819c6aea078692d1ee0bd6d83d189a
MuhammadBurhan-2006 <burhanyousaf206@gmail.com>
2026-03-18 20:39:43 +0500

    Globals are updated as the globals which we point outs for common use are added for the part of Abdul Rehman

---
+++ CartManagementSystem/CartManagementSystem/Globals.h
@@ -0,0 +1,13 @@
+#ifndef GLOBAL_H
+#define GLOBALS_H
+#include<string>
+using namespace std;
+//File paths for the data handling basically would be used by Abdul Rehman
+const string FILE_PRODUCTS = "data/products.txt";
+const string FILE_CUSTOMERS = "data/customers.txt";
+const string FILE_ADMINS = "data/admins.txt";
+const string FILE_SALES_LOG = "data/sales_log.txt";
+const string FILE_RECEIPTS_DIR = "data/receipts/";
+const string FILE_REPORTS_DIR = "data/reports/";
+const string FILE_REFUNDS_LOG = "data/refunds.txt";
+#endif

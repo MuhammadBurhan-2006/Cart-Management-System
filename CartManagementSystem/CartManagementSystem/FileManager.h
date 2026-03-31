8926272c72c789e5602e5b775c6bbb7228b12cbb
gitabdulhub123 <abdulrehmanvirk01@gmail.com>
2026-03-31 17:09:57 +0500

    Declaring Classes in FileManager.h

--- CartManagementSystem/CartManagementSystem/FileManager.h
+++ CartManagementSystem/CartManagementSystem/FileManager.h
@@ -1,5 +1,16 @@
-hello world
+#ifndef FILEMANAGER_H
+#define FILEMANAGER_H
+
+#include<iostream>
+#include<fstream>
+#include "globals.h"
+using namespace std;
+//declaring classes
+class Product;
+class customer;
+class Admin;
+class Bill;
 
 
 
-
+endif
